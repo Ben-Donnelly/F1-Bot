0ef9b4c730c4469cca95f980367c88187a521c80
Ben D <33376747+BenD@users.noreply.github.com>
2021-09-18 22:25:59 +0100

    code cleanup, file rename, code refractoring

--- ApiCalls.py
+++ ApiCalls.py
@@ -3,7 +3,7 @@ from lxml import etree
 from datetime import datetime
 
 
-class APICalls:
+class Calls:
 	current_year = datetime.today().year
 
 	def __init__(self, year=current_year, race_number="last"):
@@ -14,15 +14,16 @@ class APICalls:
 
 	def validate_parameters(self):
 		if self.race_number == 'last':
-			self.extra_information = f"You did not specifY a race number so I got the information for the last race\n"
+			self.extra_information = f"You did not specify a race number, so I got the information for the last race\n"
+
 	# Drivers
 	def drivers_for_year(self):
 		self.def_return_value = get(f"{self.base_url}/{self.year}/drivers")
-		return self.to_string()
+		return self
 
 	def driver_information(self, driver_id):
 		self.def_return_value = get(f"{self.base_url}/drivers/{driver_id}")
-		return self.to_string()
+		return self
 
 	# Constructors
 	def constructors_for_year(self):
@@ -32,7 +33,7 @@ class APICalls:
 		else:
 			self.def_return_value = get(f"{self.base_url}/{self.year}/constructors")
 		self.validate_parameters()
-		return self.to_string()
+		return self
 
 	# Circuts
 	def circuts_for_year(self):
@@ -44,7 +45,7 @@ class APICalls:
 		self.validate_parameters()
 
 		# TODO: Special characters (e.g. ü) print in unicode, fix
-		return self.to_string()
+		return self
 
 	# result
 	def results(self):
@@ -52,7 +53,7 @@ class APICalls:
 		# Most recent race result: replace year and race_number with current and last
 		self.def_return_value = get(f"{self.base_url}/{self.year}/{self.race_number}/results")
 		self.validate_parameters()
-		return self.to_string()
+		return self
 
 	# Seasons
 	# Ignore the type hint here
@@ -60,17 +61,17 @@ class APICalls:
 		# This has date and time
 		# Can do year = "current"
 		self.def_return_value = get(f"{self.base_url}/{self.year}")
-		return self.to_string()
+		return self
 
 	def season_list(self):
 		self.def_return_value = get(f"{self.base_url}/{self.year}")
-		return self.to_string()
+		return self
 
 	# Qualifying
 	def qualifying_results(self):
 		self.def_return_value = get(f"{self.base_url}/{self.year}/{self.race_number}/qualifying")
 		self.validate_parameters()
-		return self.to_string()
+		return self
 
 	# Standings
 	def driver_standings_after_a_race(self):
@@ -80,7 +81,7 @@ class APICalls:
 		else:
 			self.def_return_value = get(f"{self.base_url}/{self.year}/{self.race_number}/driverStandings")
 		self.validate_parameters()
-		return self.to_string()
+		return self
 
 	def constructor_standings_after_a_race(self):
 		if not self.race_number:
@@ -89,34 +90,34 @@ class APICalls:
 		else:
 			self.def_return_value = get(f"{self.base_url}/{self.year}/{self.race_number}/constructorStandings")
 		self.validate_parameters()
-		return self.to_string()
+		return self
 
 	def current_drivers_standings(self):
 		self.def_return_value = get(f"{self.base_url}/current/driverStandings")
-		return self.to_string()
+		return self
 
 	def current_constructors_standings(self):
 		self.def_return_value = get(f"{self.base_url}/current/constructorStandings")
-		return self.to_string()
+		return self
 
 	def all_winners_of_drivers_championships(self):
 		# TODO: starts at 1950, limit = datetime current year - 1950, can offset it too and can change finishing position
 		self.def_return_value = get(f"{self.base_url}/driverStandings/1?limit=25&offset=45")
-		return self.to_string()
+		return self
 
 	def all_winners_of_constructors_championships(self):
 		# Can offset it too and can change limit
 		self.def_return_value = get(f"{self.base_url}/constructorStandings/1")
-		return self.to_string()
+		return self
 
 	def driver_standings_by_specifying_the_driver(self, driver_id):
 		self.def_return_value = get(f"{self.base_url}/drivers/{driver_id}/driverStandings")
-		return self.to_string()
+		return self
 
 	def constructor_standings_by_specifying_the_constructor(self, constructor_id):
 		# Could limit 1 here to get first year team was in f1
 		self.def_return_value = get(f"{self.base_url}/constructors/{constructor_id}/constructorStandings")
-		return self.to_string()
+		return self
 
 	# Finishing status
 	def list_of_all_finishing_status_codes(self):
@@ -125,16 +126,16 @@ class APICalls:
 		# Could merge with 2 methods below
 
 		self.def_return_value = get(f"{self.base_url}/status?limit=150")
-		return self.to_string()
+		return self
 
 	def list_of_finishing_status_for_a_specific_season(self):
 		self.def_return_value = get(f"{self.base_url}/{self.year}/status")
-		return self.to_string()
+		return self
 
 	def list_of_finishing_status_for_a_specific_race_number_in_a_season(self):
 		self.def_return_value = get(f"{self.base_url}/{self.year}/{self.race_number}/status")
 		self.validate_parameters()
-		return self.to_string()
+		return self
 
 	# Lap times
 	def lap_times(self, lapnumber=None):
@@ -146,30 +147,34 @@ class APICalls:
 
 		self.def_return_value = get(get_url)
 		self.validate_parameters()
-		return self.to_string()
+		return self
 
 	# Pit stops
 	def pit_stop_data_for_a_race(self):
 		self.def_return_value = get(f"{self.base_url}/{self.year}/{self.race_number}/pitstops")
 		self.validate_parameters()
-		return self.to_string()
+		return self
 
 	def specific_pit_stop_data_for_a_race(self, pitstop_number):
 		self.def_return_value = get(f"{self.base_url}/{self.year}/{self.race_number}/pitstops/{pitstop_number}")
 		self.validate_parameters()
-		return self.to_string()
+		return self
 
-	def to_string(self):
-		to_string_return_value = etree.fromstring(self.def_return_value.content)
-		return f"{self.extra_information}{etree.tostring(to_string_return_value, pretty_print=True).decode()}"
+	@staticmethod
+	def to_string(data):
+		to_string_return_value = etree.fromstring(data.def_return_value.content)
+		return f"{data.extra_information}{etree.tostring(to_string_return_value, pretty_print=True).decode()}"
 
 	@staticmethod
 	def main():
-		api_call = APICalls(race_number=4)
-		# Next: All winners of drivers' championships
-		print(api_call.specific_pit_stop_data_for_a_race(1))
+		# noinspection PyTypeChecker
+		api_call = Calls(race_number=4)
+
+		api_call_result = api_call.drivers_for_year()
+
+		print(api_call.to_string(api_call_result))
 
 
 if __name__ == "__main__":
-	api_call = APICalls()
-	api_call.main()
+	call = Calls()
+	call.main()
